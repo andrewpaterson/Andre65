05f60c1e45f198f5f29f5998cf93e974cd41e818
Andrew Paterson <andrew.ian.paterson@gmail.com>
2024-04-16 11:13:00 +0200

    Update Rom.hex file capitalisation.

--- Tide/Video/make.bat
+++ Tide/Video/make.bat
@@ -1,5 +1,5 @@
 del *.lst *.obj
-del bin\*.bin bin\*.map bin\*.sym  
+del bin\*.bin bin\*.map bin\*.sym bin\*.hex
 
 wdc816as.exe  -g -l -DUSING_816 -DLARGE -o StartASM.obj Start.asm
 wdc816as.exe  -g -l -DUSING_816 -DLARGE -o GraphicsASM.obj Graphics.asm 
@@ -12,4 +12,4 @@ wdc816cc.exe  -bs -DUSING_816 -ml -wr -wu -so -sp -lt -pb -pp -px -o Splatter.ob
 wdc816cc.exe  -bs -DUSING_816 -ml -wr -wu -lt -pb -pp -px -o Random.obj Random.c
 wdc816cc.exe  -bs -DUSING_816 -ml -wr -wu -so -sp -lt -pb -pp -px -o Main.obj Main.c
 wdcln.exe -C10000 -D20000 -g -t -sz -HIE -obin\Video.hex Heap.obj Image.obj Circle.obj Polygon.obj Random.obj Splatter.obj ColourTest.obj Main.obj StartASM.obj GraphicsASM.obj -lcl -lml
-srec_cat.exe -IGnore-Checksums bin\Video.hex -Intel mult.hex -Intel -o bin/rom.hex -Intel
+srec_cat.exe -IGnore-Checksums bin\Video.hex -Intel mult.hex -Intel -o bin/Rom.hex -Intel
